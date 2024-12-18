8dabad21bb6921f9e20b367a5e63c132be6a332c
Krl22 <carthe5kboy@gmail.com>
2024-12-17 19:42:26 -0500

    edited firebaseconfig again, removed vite_ prefix

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -28,13 +28,13 @@ if (import.meta.env.MODE === "development") {
 } else {
   // Usar las variables de entorno definidas en Choreo para producción
   firebaseConfig = {
-    apiKey: process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FIREBASE_APP_ID,
-    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   };
 }
 
@@ -48,3 +48,5 @@ export const googleProvider = new GoogleAuthProvider();
 
 //Real time database
 export const db = getFirestore(app);
+
+console.log("Firebase API Key: ", process.env.VITE_FIREBASE_API_KEY);
